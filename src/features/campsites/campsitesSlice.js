000263a28c04a03ce4aaf79e6e6c255f3bbb0347
MK Dean <marykathryn.dean@gmail.com>
2023-08-23 20:29:27 -0400

    redux campsite slice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,12 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+const intialState = {
+    campsiteArray: CAMPSITES
+};
+
+const campsiteSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsiteSlice.reducer;
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
 };
-
-// export const selectRandomCampsite = () => {
-//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-// };
 
 export const selectCampsiteById = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
