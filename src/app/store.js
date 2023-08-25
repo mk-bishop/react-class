b3985ddc257754219f09b9eb5f3f1894b69338b6
MK Dean <marykathryn.dean@gmail.com>
2023-08-25 17:51:13 -0400

    redux for all

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { commentsReducer } from '../features/comments/commentsSlice';
+import { partnersReducer } from '../features/partners/partnersSlice';
 
 export const store = configureStore({
   reducer: {
-    campsites: campsitesReducer
+    campsites: campsitesReducer,
+    promotions: promotionsReducer,
+    partners: partnersReducer,
+    comments: commentsReducer
   }
 });
