b0ee81e37648947eb4f2f50f55da0e29390c62a7
MK Dean <marykathryn.dean@gmail.com>
2023-08-26 12:31:58 -0400

    week 4 workshop

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,15 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
     promotions: promotionsReducer,
     partners: partnersReducer,
-    comments: commentsReducer
+    comments: commentsReducer,
+    user: userReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
